ecbe66ac252427583796fa6764abe41dd681438e
JeSSiCa <JeSSiCa@ENCYCLOPEDIA>
2024-05-23 12:44:10 +0800

    Update manager and service class for distance calculation.

--- src/com/woodwing/manager/DistanceCalculatorManager.java
+++ src/com/woodwing/manager/DistanceCalculatorManager.java
@@ -1,5 +1,22 @@
 package com.woodwing.manager;
 
+import com.woodwing.bean.DistanceBean;
+import javax.jws.WebMethod;
+import javax.jws.WebService;
+import javax.jws.soap.SOAPBinding;
+
+@WebService
+@SOAPBinding(style = SOAPBinding.Style.DOCUMENT)
 public class DistanceCalculatorManager {
-    
+
+    @WebMethod
+    public Double calculate(Double distanceA, String unitA, Double distanceB, String unitB) {
+        //TODO data checking for Double distanceA and distanceB (!null) 
+        //TODO data checking for String unitA and unitB (!null, "yard", "meter")
+
+        DistanceBean distanceBeanA = new DistanceBean(distanceA, unitA);
+        DistanceBean distanceBeanB = new DistanceBean(distanceA, unitB);
+
+        return distanceBeanA.getDistanceInMeter() + distanceBeanB.getDistanceInMeter();
+    }
 }
--- src/com/woodwing/service/DistanceCalculatorService.java
+++ src/com/woodwing/service/DistanceCalculatorService.java
@@ -1,5 +1,15 @@
 package com.woodwing.service;
 
+import com.woodwing.manager.DistanceCalculatorManager;
+import javax.jws.WebService;
+import javax.jws.soap.SOAPBinding;
+import javax.xml.ws.Endpoint;
+
+@WebService
+@SOAPBinding(style = SOAPBinding.Style.DOCUMENT)
 public class DistanceCalculatorService {
-    
+
+    public static void main(String[] args) {
+        Endpoint.publish("http://localhost:8888/DistanceCalculatorWS", new DistanceCalculatorManager());
+    }
 }
