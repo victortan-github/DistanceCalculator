ab5f67e2b97c90226d7e3b7b1533949091910a24
JeSSiCa <JeSSiCa@ENCYCLOPEDIA>
2024-05-23 12:15:20 +0800

    Add utility class for yard to meter conversion.

---
+++ src/com/woodwing/util/DistanceCalculatorUtils.java
@@ -0,0 +1,11 @@
+package com.woodwing.util;
+
+public class DistanceCalculatorUtils {
+
+    private static final Double YARD_TO_METER = 0.9144;
+
+    public static Double convertYardToMeter(double distance) {
+
+        return distance * YARD_TO_METER;
+    }
+}
